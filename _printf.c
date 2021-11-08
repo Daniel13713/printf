6c0ccba42a98c286d4819105246eafbd4ea602c3
SharaGB <shara.garcia.betancur@gmail.com>
2021-11-08 13:38:43 -0500

    Fix Null

--- _printf.c
+++ _printf.c
@@ -11,8 +11,8 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	va_start(ap, format);
-	if (format == NULL)
-		exit(98);
+    if (format == NULL)
+        return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
